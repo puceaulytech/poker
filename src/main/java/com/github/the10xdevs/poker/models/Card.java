de5484fb44a75e543a4fbedd85c2439f9b3b63e8
Saad BENAQA <saad.benaqa@etu.univ-cotedazur.fr>
2023-10-28 18:10:33 +0200

    testing the method computeBestHand correctly

--- src/main/java/com/github/the10xdevs/poker/models/Card.java
+++ src/main/java/com/github/the10xdevs/poker/models/Card.java
@@ -18,7 +18,7 @@ public class Card {
 
     /**
      * Compute the best hand possible out of multiples cards
-     * @param  repr The cards the player has 
+     * @param  repr The cards the player has
      * @return The best hand
      */
 
--- src/test/java/com/github/the10xdevs/poker/models/PlayerTest.java
+++ src/test/java/com/github/the10xdevs/poker/models/PlayerTest.java
@@ -1,5 +1,6 @@
 package com.github.the10xdevs.poker.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,21 +9,24 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    private Player playerOne;
+
 
     @Test
      void computeBestHandFours() {
+        Player playerOne= Player.fromString("RCa RPi RCo RTr");
         List<Card> cards = Arrays.asList(new Card(Suit.DIAMOND, Rank.KING), new Card(Suit.SPADE, Rank.KING), new Card(Suit.CLUB, Rank.KING), new Card(Suit.HEART, Rank.KING));
-
-        Hand bestHand = Player.computeBestHand();
+        Hand bestHand = playerOne.computeBestHand();
 
         assertEquals(HandType.FOUR_OF_A_KIND, bestHand.getType());
         assertEquals(Rank.KING, bestHand.getHighestRank());
     }
     @Test
     void computeBestHandThrees() {
-        List<Card> cards = Arrays.asList(new Card(Suit.DIAMOND, Rank.KING), new Card(Suit.SPADE, Rank.KING), new Card(Suit.CLUB, Rank.KING));
 
-        Hand bestHand = Player.computeBestHand();
+        List<Card> cards = Arrays.asList(new Card(Suit.DIAMOND, Rank.KING), new Card(Suit.SPADE, Rank.KING), new Card(Suit.CLUB, Rank.KING));
+        Player playerOne= Player.fromString("RCa RPi RCo ");
+        Hand bestHand = playerOne.computeBestHand();
 
         assertEquals(HandType.THREE_OF_A_KIND, bestHand.getType());
         assertEquals(Rank.KING, bestHand.getHighestRank());
@@ -31,8 +35,8 @@ class PlayerTest {
     @Test
     void computeBestHandDoublePairs() {
         List<Card> cards = Arrays.asList(new Card(Suit.DIAMOND, Rank.KING), new Card(Suit.SPADE, Rank.KING), new Card(Suit.CLUB, Rank.FIVE), new Card(Suit.DIAMOND, Rank.FIVE));
-
-        Hand bestHand = Player.computeBestHand();
+        Player playerOne= Player.fromString("RCa RPi 5Ca 5Tr");
+        Hand bestHand = playerOne.computeBestHand();
 
         assertEquals(HandType.DOUBLE_PAIR, bestHand.getType());
         assertEquals(Rank.KING, bestHand.getHighestRank());
@@ -41,8 +45,8 @@ class PlayerTest {
     @Test
     void computeBestHandPairs() {
         List<Card> cards = Arrays.asList(new Card(Suit.DIAMOND, Rank.KING), new Card(Suit.SPADE, Rank.KING), new Card(Suit.CLUB, Rank.FIVE));
-
-        Hand bestHand = Player.computeBestHand();
+        Player playerOne= Player.fromString("RCa RPi 5Co ");
+        Hand bestHand = playerOne.computeBestHand();
 
         assertEquals(HandType.PAIR, bestHand.getType());
         assertEquals(Rank.KING, bestHand.getHighestRank());
@@ -51,8 +55,8 @@ class PlayerTest {
     @Test
     void computeBestHandHighestRank() {
         List<Card> cards = Arrays.asList(new Card(Suit.DIAMOND, Rank.JACK), new Card(Suit.SPADE, Rank.TEN), new Card(Suit.CLUB, Rank.FIVE));
-
-        Hand bestHand = Player.computeBestHand();
+        Player playerOne= Player.fromString("VCa 10Pi 5Co ");
+        Hand bestHand = playerOne.computeBestHand();
 
         assertEquals(HandType.HIGH_CARD, bestHand.getType());
         assertEquals(Rank.JACK, bestHand.getHighestRank());
