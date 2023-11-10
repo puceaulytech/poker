5431ccba6685d35993bb9b9fca7222bf23f35ecf
QwEekYhyo <logan.lucas@etu.univ-cotedazur.fr>
2023-11-10 12:59:38 +0100

    feat: cool banner when starting app

--- src/main/java/com/github/the10xdevs/poker/Main.java
+++ src/main/java/com/github/the10xdevs/poker/Main.java
@@ -7,11 +7,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static void printUsage() {
+        System.out.println("""
+                \033[1m---- Bienvenue au jeu de Poker ----\033[0m
+                \033[4mComment entrer une valeur:\033[0m
+                Nombre ou initiale de la tête (V, D, R, A)
+                \033[4mComment entrer une couleur:\033[0m
+                \033[1;90m♠\033[0m Piques  : Pi
+                \033[1;31m♥\033[0m Coeurs  : Co
+                \033[1;90m♣\033[0m Trèfles : Tr
+                \033[1;31m♦\033[0m Carreaux: Ca
+                \033[4mExemple: 10Pi\033[0m
+                """);
+    }
 
     public static void main(String[] args) {
+        printUsage();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        for (;;) {
+        for (; ; ) {
             Player playerOne = Player.readFromTerminal(reader);
             Player playerTwo = Player.readFromTerminal(reader);
             Duel game = new Duel(playerOne, playerTwo);
